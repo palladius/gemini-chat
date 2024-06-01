7204726613c460be74ca24e7d843d85a9173512e
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-01 13:01:45 +0200

    0.2.1 pre CSS revolution

--- gemini-chat/app/controllers/rooms_controller.rb
+++ gemini-chat/app/controllers/rooms_controller.rb
@@ -1,9 +1,14 @@
 class RoomsController < ApplicationController
 
+  def create
+    @room = Room.create(name: params["room"]["name"])
+  end
+
   def index
     @current_user = current_user
     redirect_to '/signin' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(@current_user)
+    @room = Room.new
   end
 end
