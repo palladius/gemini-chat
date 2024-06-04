b15aa8a73e485d6280f37e13a916cde755c026f1
Riccardo Carlesso <palladiusbonton@gmail.com>
2024-06-04 08:27:26 +0200

    Update gemini-chat version to 0.5.14c and add PID to job

--- gemini-chat/app/jobs/gemini_room_reply_forever_job.rb
+++ gemini-chat/app/jobs/gemini_room_reply_forever_job.rb
@@ -8,14 +8,16 @@
 class GeminiRoomReplyForeverJob < ApplicationJob
   queue_as :default
 
+  # TODO also create the only once job, to test with CRun.
   def perform(*args)
     # Do something later
     #sleep_time ||= args[:sleep_time]
     job_version = '1.0'
     sleep_distance = 3
+    pid = $$ # Process.pid
     while true
       puts '⏳' * 80
-      puts "⏳♊️ Gemini ReplyAll 4ever Job v#{job_version} - every #{sleep_distance} seconds: #{Time.now}"
+      puts "⏳♊️ [PID=#{pid}] Gemini ReplyAll 4ever Job v#{job_version} - every #{sleep_distance} seconds: #{Time.now}"
       puts '⏳' * 80
       Room.gemini_reply_all
       sleep(sleep_distance)
